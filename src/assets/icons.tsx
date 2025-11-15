786fa6568e9b488af6c26957e4b32081cd1cf22a
Matheus Paixão Silva <matpaixaoo@outlook.com>
2025-11-15 00:20:55 -0300

    feat: add classroom and solara images for branding

---
+++ src/assets/icons.tsx
@@ -0,0 +1,83 @@
+import { RiHome2Fill } from 'react-icons/ri';
+import { FaUsers, FaUser } from 'react-icons/fa';
+import { IoArrowBack } from 'react-icons/io5';
+import {
+  BiSolidRightArrow,
+  BiSolidDownArrow,
+  BiSolidUpArrow,
+  BiSolidLeftArrow,
+} from 'react-icons/bi';
+import { FaClock } from 'react-icons/fa';
+import { FaRegTrashCan } from 'react-icons/fa6';
+import { MdModeEditOutline } from 'react-icons/md';
+import { FaTrash } from 'react-icons/fa';
+import { FaCopy } from 'react-icons/fa';
+import { IoIosArrowBack } from 'react-icons/io';
+import { MdAddBox } from 'react-icons/md';
+import { FaEdit } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa6';
+import { FaSearch } from 'react-icons/fa';
+import { IoClose } from 'react-icons/io5';
+import { TbDotsVertical } from 'react-icons/tb';
+import { FaSave } from 'react-icons/fa';
+import { FaChalkboardTeacher } from 'react-icons/fa';
+import { HiAcademicCap } from 'react-icons/hi2';
+import { FaBook } from 'react-icons/fa';
+import { MdAssignment } from 'react-icons/md';
+import { IoLogOutSharp } from 'react-icons/io5';
+import { IoCalendar } from 'react-icons/io5';
+import { FaCircleExclamation } from 'react-icons/fa6';
+
+import {
+  Check as CheckIcon,
+  ClipboardList as ClipboardListIcon,
+  X as XIcon,
+  Eye as EyeIcon,
+  EyeOff as EyeOffIcon,
+  Loader2 as LoaderIcon,
+  UserPlus as UserPlusIcon,
+  UserMinus as UserMinusIcon,
+  LayoutDashboard as DashboardIcon,
+  MoreHorizontal as MoreHorizontalIcon,
+} from 'lucide-react';
+
+export {
+  IoLogOutSharp as Logout,
+  MdAssignment as Assignment,
+  FaBook as Book,
+  HiAcademicCap as Student,
+  FaChalkboardTeacher as Teacher,
+  RiHome2Fill as Home,
+  IoCalendar as Calendar,
+  FaUser as User,
+  FaUsers as Users,
+  IoArrowBack as Return,
+  BiSolidUpArrow as UpArrow,
+  BiSolidRightArrow as RightArrow,
+  BiSolidDownArrow as DownArrow,
+  BiSolidLeftArrow as LeftArrow,
+  FaClock as Clock,
+  FaRegTrashCan as Trash,
+  MdModeEditOutline as Pencil,
+  FaTrash as TrashAlt,
+  FaCopy as Copy,
+  IoIosArrowBack as ReturnAlt,
+  MdAddBox as AddBox,
+  FaEdit as Edit,
+  FaPlus as PlusSign,
+  FaSearch as Search,
+  IoClose as Cross,
+  TbDotsVertical as VDots,
+  FaSave as Save,
+  FaCircleExclamation as Exclamation,
+  CheckIcon as Check,
+  ClipboardListIcon as ClipboardList,
+  XIcon as X,
+  EyeIcon as Eye,
+  EyeOffIcon as EyeOff,
+  LoaderIcon as Loader,
+  UserPlusIcon as UserPlus,
+  UserMinusIcon as UserMinus,
+  DashboardIcon as Dashboard,
+  MoreHorizontalIcon as MoreHorizontal,
+};
